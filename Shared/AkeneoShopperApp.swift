0e05f3dbaee260081e6052981682ea8b5a37fcba
Julien Sanchez <sumbobyboys@gmail.com>
2021-01-15 16:00:10 +0100

    Initial Commit

---
+++ Shared/AkeneoShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkeneoShopperApp.swift
+//  Shared
+//
+//  Created by Julien Sanchez on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AkeneoShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
